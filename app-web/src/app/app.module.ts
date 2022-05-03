1a05cf5d54a6f7c5f3664291d73f7abb10212ed1
Oliveira <caroline.oliveira@capgemini.com>
2022-05-03 09:25:14 -0300

    adicionei o arquivo do back end em nodejs

--- app-web/src/app/app.module.ts
+++ app-web/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlunoComponent } from './aluno/aluno.component';
+import { AulaComponent } from './aula/aula.component';
+import { EscolaComponent } from './escola/escola.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunoComponent,
+    AulaComponent,
+    EscolaComponent
   ],
   imports: [
     BrowserModule,

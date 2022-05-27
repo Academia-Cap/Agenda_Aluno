2ddc03de4a2e798fda1063a403c4a96ad4e7bb48
Rocha <tania.rocha@capgemini.com>
2022-05-27 15:32:19 -0300

    Crud completo Instituicao

--- app-web/src/app/instituicao-servico/instituicao.service.ts
+++ app-web/src/app/instituicao-servico/instituicao.service.ts
@@ -9,24 +9,29 @@ export class InstituicaoService {
   constructor(private http: HttpClient) { }
 
   gravar(dados: any){
-    let url = 'http://localhost:8000/instituicao'
-    return this.http.post(url, dados)
+    //return this.http.post('http://localhost:8000/instituicao', dados)
+    return this.http.post('http://localhost:8080/instituicao', dados)
   }
 
   getTodos(){
-   return this.http.get('http://localhost:8000/instituicao')
+   //return this.http.get('http://localhost:8000/instituicao')
+   return this.http.get('http://localhost:8080/instituicao')
   }
 
   excluir(id: number){
-    return this.http.delete(`http://localhost:8000/instituicao/${id}`)
+    //return this.http.delete(`http://localhost:8000/instituicao/${id}`)
+    console.log(id)
+    return this.http.delete(`http://localhost:8080/instituicao/${id}`)
   }
 
   getId(id: number){
-    return this.http.get(`http://localhost:8000/instituicao/${id}`)
+    //return this.http.get(`http://localhost:8000/instituicao/${id}`)
+    return this.http.get(`http://localhost:8080/instituicao/${id}`)
   }
 
   alterar(dados:any){
-    return this.http.put(`http://localhost:8000/instituicao/${dados.id}`, dados)
+    //return this.http.put(`http://localhost:8000/instituicao/${dados.id}`, dados)
+    return this.http.put(`http://localhost:8080/instituicao/${dados.id}`, dados)
   }
   
 }

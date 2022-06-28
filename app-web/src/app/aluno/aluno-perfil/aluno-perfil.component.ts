bcd29051d75090586989e5c5eaea77e7f65b7af3
TaniaRocha <roc.ta.cris@gmail.com>
2022-06-28 08:07:50 -0300

    Adicionando avatar

--- app-web/src/app/aluno/aluno-perfil/aluno-perfil.component.ts
+++ app-web/src/app/aluno/aluno-perfil/aluno-perfil.component.ts
@@ -4,6 +4,7 @@ import { AlunoService } from '../aluno-servico/aluno.service';
 import { ValidarCamposService } from '../aluno-servico/validar-campos.service';
 import { DecodeTokenService } from '../aluno-servico/autenticacao/decode-token.service';
 import { InformacaoService } from '../aluno-servico/informacao.service';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -17,12 +18,26 @@ export class AlunoPerfilComponent implements OnInit {
   msgTel: string = '';
   msgLogin: string = '';
   aluno: any = {};
+  imagem: any = {'title':'', 'description':'', 'image': ''}
+  avatar: any = {'image':''}
+  idavatar: any;
   idAluno: number = 0
   alunoToken: any;
   displayStyle: any;
   block = false;
 
-  constructor(private servicoAluno: AlunoService, private router: Router, 
+  url:any;
+
+  image: string = '';
+
+  imageHash:any;
+  link:any;
+
+  shortLink: string = "";
+  loading: boolean = false; // Flag variable
+  file: File | undefined; // Variable to store file
+
+  constructor(private servicoAluno: AlunoService, private router: Router, private http: HttpClient,
     private serviceValidar: ValidarCamposService, private decodeToken: DecodeTokenService, private displayInfo: InformacaoService) {
     this.alunoToken = this.decodeToken.decodeTokenJWT()
     this.idAluno = this.alunoToken.id
@@ -31,6 +46,7 @@ export class AlunoPerfilComponent implements OnInit {
   }
   ngOnInit(): void {
    this.displayStyle = this.displayInfo.getDisplayInfo()
+   this.viewImagem()
   }
 
   closeInfo() {
@@ -63,5 +79,50 @@ export class AlunoPerfilComponent implements OnInit {
   Deslogar() {
     sessionStorage.removeItem('token')
     this.router.navigate(['/']);
+  }
+
+  addImagem(){
+    /*console.log(event);
+
+    const selectedFiles = event.srcElement.files;
+    
+    this.imagem.title = this.aluno.nome
+    this.imagem.description = "Foto de "+this.aluno.nome
+    this.imagem.image = 
+
+    this.servicoAluno.uploadImgur(this.imagem).subscribe(x =>{ 
+      this.imagem = x
+      console.log(this.imagem)
+      this.idavatar = this.imagem.id
+      this.servicoAluno.addAvatar(this.idavatar,this.idAluno)
+    })*/
+    //this.servicoAluno.uploadImgur().subscribe(x=> console.log(x))
+  }
+  
+onChange(event: any) {
+    this.file = event.target.files[0];
+}
+
+onUpload() {
+  this.servicoAluno.upload(this.file).subscribe(x => console.log(x));
+}
+
+  viewImagem(){
+    this.servicoAluno.viewAvatar(this.idAluno).subscribe(x =>{
+      this.imageHash = x
+      this.link = "https://i.imgur.com/"+this.imageHash.idavatar+".png"
+      this.image = this.link
+    })
+
+  }
+
+  excluirImagem(){
+    this.servicoAluno.viewAvatar(this.idAluno).subscribe(x =>{
+      this.imageHash = x
+      this.link = "https://api.imgur.com/3/image/"+this.imageHash.idavatar
+      this.servicoAluno.deleteImgur(this.link)
+      this.idavatar =  "qsHobpa"
+      this.servicoAluno.deleteAvatar(this.idAluno, this.idavatar).subscribe(x => this.imagem = x)
+    })
   }
 }
--- app-web/src/app/aluno/aluno-servico/aluno.service.ts
+++ app-web/src/app/aluno/aluno-servico/aluno.service.ts
@@ -1,39 +1,70 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlunoService {
+  baseApiUrl = "https://api.imgur.com/3/image/"
+
 
   constructor(private http: HttpClient) { }
 
-  gravar(dados: any){
+  gravar(dados: any) {
     let url = `http://localhost:8000/aluno`
-    return this.http.post(url,dados)
+    return this.http.post(url, dados)
   }
 
-  getAluno(idAluno: number){
+  getAluno(idAluno: number) {
     let url = `http://localhost:8000/aluno/${idAluno}`
     return this.http.get(url)
   }
 
-  alterarAluno(id: number, dados: any){
+  alterarAluno(id: number, dados: any) {
     let url = `http://localhost:8000/aluno/${id}`
     return this.http.put(url, dados)
   }
 
-  deletarAluno(idAluno: number){
+  deletarAluno(idAluno: number) {
     let url = `http://localhost:8000/aluno/${idAluno}`
     return this.http.get(url)
   }
 
-  alterarSenha(dados: any, idAluno: number){
+  alterarSenha(dados: any, idAluno: number) {
     let url = `http://localhost:8000/aluno/alterarSenha/${idAluno}`
     return this.http.put(url, dados)
   }
 
-  info(){
 
+  //--------------------------------------------
+  deleteImgur(dados: any) {
+    console.log(dados)
+    return this.http.post(dados, null, { headers: { "Authorization": ["Barer 57cb94391f26f835f9970935ef613749b0f7c646"] } })
+  }
+
+  deleteAvatar(idAluno: any, idAvatar: any){
+    let url = `http://localhost:8000/aluno/addAvatar/${idAluno}`
+    return this.http.put(url, idAvatar)
+  }
+
+  viewAvatar(idAluno: number) {
+    let url = `http://localhost:8000/aluno/viewAvatar/${idAluno}`
+    return this.http.get(url)
+  }
+
+  addAvatar(idAluno: any, dados: any) {
+    let url = `http://localhost:8000/aluno/addAvatar/${idAluno}`
+    return this.http.put(url, dados)
+  }
+
+  upload(file: any): Observable<any> {
+    const formData = new FormData();
+    formData.append("file",file)
+    console.log(formData)
+    return this.http.post(`https://api.imgur.com/3/image/`, formData, { headers: { "Authorization": ["Bearer 57cb94391f26f835f9970935ef613749b0f7c646"] } })
   }
 }
+
+

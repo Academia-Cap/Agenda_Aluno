e0d65c58a72c92d9f3d0d5f8098045b3e34dbb19
Caroline <carolinelara35@gmail.com>
2022-06-29 13:44:16 -0300

    Alterei a rota de login serviço local para o heroku.

--- app-web/src/app/aluno/aluno-servico/autenticacao/authentication.service.ts
+++ app-web/src/app/aluno/aluno-servico/autenticacao/authentication.service.ts
@@ -9,7 +9,7 @@ export class AuthenticationService {
   constructor(private http: HttpClient) { }
 
   logar(email: string, senha: string){
-    return this.http.post('http://localhost:8000/alunoLogin/login',{
+    return this.http.post('https://agendaaluno.herokuapp.com/alunoLogin/login',{
       email: email,
       senha: senha
     })

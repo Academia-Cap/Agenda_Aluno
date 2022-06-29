c851780d2e2a602886942ce96e32869026248cda
Caroline <carolinelara35@gmail.com>
2022-06-29 13:47:14 -0300

    Alterei a rota de calendario serviço local para o heroku.

--- app-web/src/app/calendario/calendario-servico/calendario.service.ts
+++ app-web/src/app/calendario/calendario-servico/calendario.service.ts
@@ -9,27 +9,27 @@ export class CalendarioService {
   constructor(private http: HttpClient) { }
 
   gravar(dados: any){
-    return this.http.post('https://agendaaluno.herokuapp.com', dados)
+    return this.http.post('https://agendaaluno.herokuapp.com/calendario', dados)
   }
 
   excluir(id: number){
-    return this.http.delete(`https://agendaaluno.herokuapp.com/${id}`)
+    return this.http.delete(`https://agendaaluno.herokuapp.com/calendario/${id}`)
   }
 
   getTarefa(data: any){
-    return this.http.post(`https://agendaaluno.herokuapp.com/periodo`, data)
+    return this.http.post(`https://agendaaluno.herokuapp.com/calendario/periodo`, data)
   }
 
   alterar(dados:any){
-    return this.http.put(`https://agendaaluno.herokuapp.com/${dados.id}`, dados)
+    return this.http.put(`https://agendaaluno.herokuapp.com/calendario/${dados.id}`, dados)
   }
 
   getDias(data: any){
-    let url = `https://agendaaluno.herokuapp.com/gerarDias`
+    let url = `https://agendaaluno.herokuapp.com/calendario/gerarDias`
     return this.http.post(url, data)
   }
 
   getId(id: number){
-    return this.http.get(`https://agendaaluno.herokuapp.com/calendario/${id}`)
+    return this.http.get(`https://agendaaluno.herokuapp.com/calendario/calendario/${id}`)
   }  
 }

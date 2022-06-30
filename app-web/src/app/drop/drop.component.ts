6090f8859318894ce126d37321395c28966486fb
TaniaRocha <roc.ta.cris@gmail.com>
2022-06-30 13:41:54 -0300

    Ajustando implementação avatar

--- app-web/src/app/drop/drop.component.ts
+++ app-web/src/app/drop/drop.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router } from '@angular/router';
+import { AlunoService } from '../aluno/aluno-servico/aluno.service';
 import { DecodeTokenService } from '../aluno/aluno-servico/autenticacao/decode-token.service';
 
 
@@ -10,15 +11,30 @@ import { DecodeTokenService } from '../aluno/aluno-servico/autenticacao/decode-t
 })
 export class DropComponent implements OnInit {
   alunoToken: any;
-  constructor(private decodeToken: DecodeTokenService, private router: Router) { }
+  idAluno: any;
+  imageHash: any;
+  link: string ='';
+  image: string='';
+
+  constructor(private decodeToken: DecodeTokenService, private router: Router,private servicoAluno: AlunoService) { }
 
   ngOnInit(): void {
-    this.alunoToken = this.decodeToken.decodeTokenJWT()  
+    this.alunoToken = this.decodeToken.decodeTokenJWT()
+    this.idAluno = this.alunoToken.id  
+    this.viewImagem()
   }
 
   Deslogar() {
     sessionStorage.removeItem('token')
     this.router.navigate(['/']);
   }
-  
+
+  viewImagem(){
+    this.servicoAluno.viewAvatar(this.idAluno).subscribe(x =>{
+      this.imageHash = x
+      this.link = "https://i.imgur.com/"+this.imageHash.idavatar+".png"
+      this.image = this.link
+    })
+  }
+
 }

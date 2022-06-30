62ef26576a9a7896c0cb0daa3cbb4f0245c78a2a
TaniaRocha <roc.ta.cris@gmail.com>
2022-06-30 13:48:20 -0300

    Atualizando funções no serviço avatar perfil aluno

--- app-web/src/app/aluno/aluno-servico/aluno.service.ts
+++ app-web/src/app/aluno/aluno-servico/aluno.service.ts
@@ -34,17 +34,6 @@ export class AlunoService {
     return this.http.put(url, dados)
   }
 
-
-  //--------------------------------------------
-  deleteImgur(dados: any) {
-    return this.http.post(dados, null, { headers: { "Authorization": ["Barer 57cb94391f26f835f9970935ef613749b0f7c646"] } })
-  }
-
-  deleteAvatar(idAluno: any, idAvatar: any){
-    let url = `http://localhost:8000/aluno/addAvatar/${idAluno}`
-    return this.http.put(url, idAvatar)
-  }
-
   viewAvatar(idAluno: number) {
     let url = `http://localhost:8000/aluno/viewAvatar/${idAluno}`
     return this.http.get(url)
